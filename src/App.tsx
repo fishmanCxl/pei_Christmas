788baa72e7b2e54cde470b8d1b1d04b8b6ec56f6
v_cxlgcchen <v_cxlgcchen@tencent.com>
2025-12-25 09:58:15 +0800

    fix: 模型本地下载

--- src/App.tsx
+++ src/App.tsx
@@ -467,7 +467,7 @@ const GestureController = ({ onGesture, onMove, onStatus, debugMode }: any) => {
         const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm");
         gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
           baseOptions: {
-            modelAssetPath: "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
+            modelAssetPath: "./models/gesture_recognizer.task",
             delegate: "GPU"
           },
           runningMode: "VIDEO",
